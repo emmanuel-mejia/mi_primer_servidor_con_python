dd0afa2e01195721aaeb774d4af8fe26419bfbe0
Emmanuel Mejia <emmanuel.mejia.jimenez@gmail.com>
2024-01-31 16:07:23 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,32 @@
 from jinja2 import Environment
 from jinja2 import FileSystemLoader
 
+
 from wsgiref.simple_server import make_server
 
+
 env = Environment(loader=FileSystemLoader('templates'))
+
 
 def aplication(environ, start_response): # Interfaz - WSGI 
     status = '200 OK'
 
+    
     context = {
         'username': 'Cody',
         'courses': ['Python', 'Django', 'Flask', 'Base de datos']
     }
+
     
     path = environ.get('PATH_INFO')
+
     
     if path == '/':
         template = env.get_template('index.html')
     
     elif path == '/users':
         template = env.get_template('users.html')
+
     
     response = template.render(**context)
     start_response(status, []) # MetaData
@@ -31,4 +38,5 @@ PORT = 4000
 with make_server('localhost', PORT, aplication) as server:
     print(f">>> El servidor se encuentra a la escucha en el puerto {PORT}")
     server.serve_forever()
+
 
